fea63a11951541c1a5009bd61ebd456df77c41c5
Gitmarkkkk <mlp23061@gmail.com>
2024-09-19 10:56:34 -0500

    mark.java

--- javaapp/src/javaapp/mark.java
+++ javaapp/src/javaapp/mark.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package javaapp;
 
-/**
- *
- * @author wrl123u
- */
+package javaapp;
+import java.util.Scanner;
 public class mark {
+    static Scanner sc;
+    static double mtod, gp, mpg, pf, tf;
     
+    public static void main(String[] args)
+    {
+        sc= new Scanner (System.in);
+        CalcExp mark = new CalcExp();
+        System.out.println("Enter Miles Driven: ");
+        mtod=sc.nextDouble();
+        System.out.println("Enter Gas Price: ");
+        gp=sc.nextDouble();
+        System.out.println("Enter MPG: ");
+        mpg=sc.nextDouble();
+        System.out.println("Enter Parking Fees: ");
+        pf=sc.nextDouble();
+        System.out.println("Enter Toll Fees: ");
+        tf=sc.nextDouble();
+        mark.CalcTG();
+        mark.CalcTGE();
+        mark.CalcTE();
+        System.out.println("Your Gallons of Gas is:");
+        System.out.println("");
+        System.out.println("");
+        
+        
+    }
 }
